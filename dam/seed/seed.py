19ba7cb6445579a6079cccfab091392fed2caee5
mrfreddi007 <mrfreddi007@hotmail.no>
2021-11-06 19:26:29 +0100

    dam writeups

---
+++ dam/seed/seed.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import sys
+import time
+import random
+import hashlib
+
+def seed():
+    a = round(time.time())
+    print(a)
+    return a
+
+def hash(text):
+    return hashlib.sha256(str(text).encode()).hexdigest()
+
+def main():
+    s = 1636222760
+    while True:
+        for i in range(10000000):
+            s = s - 1
+            random.seed(s, version=2)
+            x = random.random()
+            x = 0.016879293473860302
+            flag = hash(x)
+
+            if 'b9ff3ebf' in flag:
+                with open("./flag", "w") as f:
+                    f.write(f"dam{{{flag}}}")
+                f.close()
+                break
+            break
+            print(f"Incorrect: {x}")
+    print("Good job <3")
+
+if __name__ == "__main__":
+   sys.exit(main())
